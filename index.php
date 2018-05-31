e46ffb9913be3d58c3d931872771efe6442f4578
Yulwei <37070611+Yulwei@users.noreply.github.com>
2018-05-31 11:28:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if($method == 'POST'){
 
 	$city = $json->queryResult->parameters->geo-city;
     
-    	$url = 'http://api.openweathermap.org/data/2.5/weather?q=' + $city + '&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
+    	$url = "http://api.openweathermap.org/data/2.5/weather?q= . $city . &units=metric&appid=91418790fd46c207c3a5fa5b1411018a";
     
     	$requestWeather = file_get_contents($url);
 	$jsonWeather = json_decode($requestBody);
@@ -18,11 +18,12 @@ if($method == 'POST'){
    	$desc = $jsonWeather->weather->description;
    	$name = $jsonWeather->name
     
-    	$speech = "It looks like in " + $name + " it's " + $temp + " degrees, with " + $desc + ".";
+    	$speech = "It looks like in " . $name . " it's " . $temp . " degrees, with " . $desc + ".";
 
 	$response = new \stdClass();
 	$response->fulfillmentText = $speech;
 	$response->source = "webhook";
+	
 	echo json_encode($response);
 }
 else
