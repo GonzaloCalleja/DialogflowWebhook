0624c90e14acf0ff498b88242e0815f6fb7bece7
Yulwei <37070611+Yulwei@users.noreply.github.com>
2018-06-01 00:14:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,16 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
     
-    	$text = $json->queryResult->parameters->text;
+    	$city = $json->queryResult->parameters->geocity;
    
-    	$url = 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
+    	$url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city . '&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
     
     	$requestWeather = file_get_contents($url);
-	
 	$jsonWeather = json_decode($requestWeather);
 	
 	$response = new \stdClass();
 	$response->fulfillmentText = $jsonWeather->name;
-	$response->test = $text;
+	$response->test = $city;
 	
 	echo json_encode($response);
 }
