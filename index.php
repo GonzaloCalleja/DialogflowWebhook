f7850ce7c00ebd83f871c93f4cfda0f09c27a94b
Yulwei <37070611+Yulwei@users.noreply.github.com>
2018-05-31 23:50:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,17 +12,10 @@ if($method == 'POST'){
     	$url = 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
     
     	$requestWeather = file_get_contents($url);
+	
 	$jsonWeather = json_decode($requestWeather);
 
-   	$name = $jsonWeather['name'];
-    
-    	$speech = $name . $city;
-
-	$response = new \stdClass();
-	$response->fulfillmentText = $speech;
-	$response->source = "webhook";
-	
-	echo json_encode($response);
+	echo $jsonWeather;
 }
 else
 {
