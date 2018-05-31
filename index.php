b80c6177f662053639bbb9a850d05565cbe46306
Yulwei <37070611+Yulwei@users.noreply.github.com>
2018-05-31 12:42:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,13 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	
-	print_r($json);
     
     	$url = 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
     
     	$requestWeather = file_get_contents($url);
 	$jsonWeather = json_decode($requestWeather);
-	print_r($jsonWeather);
 
-   	$name = $jsonWeather->name
+   	$name = $jsonWeather['name'];
     
     	$speech = $name;
 
