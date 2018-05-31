5904c4c3724a1bfe98add780b88cbb79cef07e9d
Yulwei <37070611+Yulwei@users.noreply.github.com>
2018-05-31 12:03:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($method == 'POST'){
     	$url = 'http://api.openweathermap.org/data/2.5/weather?q=Madrid&units=metric&appid=91418790fd46c207c3a5fa5b1411018a';
     
     	$requestWeather = file_get_contents($url);
-	$jsonWeather = json_decode($requestBody);
+	$jsonWeather = json_decode($requestWeather);
     
    	$temp = $jsonWeather->main->temp;
    	$desc = $jsonWeather->weather->description;
